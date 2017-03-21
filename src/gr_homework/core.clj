7bf8afec2352b69f2e75715a9647d6aab49e0caf
Nerve <nervecenter7@gmail.com>
2017-03-20 21:38:49 -0400

    Sample files. Up to parsing records all as strings.

--- src/gr_homework/core.clj
+++ src/gr_homework/core.clj
@@ -1,5 +1,41 @@
 (ns gr-homework.core
+	(:use [clojure.tools.namespace.repl :only (refresh)])
+	(:require [clojure.string :as str])
   (:gen-class))
+
+(defn determine-separator
+	[line]
+	(loop [line-left line]
+		(condp = (first line-left)
+			\| \| 
+			\, \, 
+			\space \space
+			nil (throw (Exception. "No or unrecognized separators in record."))
+			(recur (rest line-left)))))
+
+(defn parse-date
+	[date-str]
+	date-str)
+
+(defn parse-record
+	[line]
+	(let [entries (->> (determine-separator line)
+										 (str)
+										 (re-pattern)
+										 (str/split line))]
+		(if (not= (count entries) 5)
+			(throw (Exception. "Invalid number of entries in record."))
+			{:last (entries 0)
+			 :first (entries 1)
+			 :gender (str/lower-case (entries 2))
+			 :fav-color (entries 3)
+			 :dob (parse-date (entries 4))})))
+
+(defn parse-records-file
+	[filename]
+	(->> (slurp filename)
+			 (str/split-lines)
+			 (map parse-record)))
 
 (defn -main
   "I don't do a whole lot ... yet."
