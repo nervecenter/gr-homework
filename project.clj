6a4b6ee7af8d6d8072eed1234ad8ebea1b2a4c3d
Nerve <nervecenter7@gmail.com>
2017-03-20 22:21:54 -0400

    Parsing done, clj-time is in.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-								 [org.clojure/tools.namespace "0.2.11"]]
+								 [org.clojure/tools.namespace "0.2.11"]
+								 [clj-time "0.13.0"]]
   :main ^:skip-aot gr-homework.core
 	:resource-paths ["resources"]
   :target-path "target/%s"
--- src/gr_homework/core.clj
+++ src/gr_homework/core.clj
@@ -1,41 +1,45 @@
 (ns gr-homework.core
 	(:use [clojure.tools.namespace.repl :only (refresh)])
-	(:require [clojure.string :as str])
+	(:require [clojure.string :as str]
+						[clj-time.core :as t])
   (:gen-class))
 
 (defn determine-separator
 	[line]
 	(loop [line-left line]
 		(condp = (first line-left)
-			\| \| 
-			\, \, 
-			\space \space
-			nil (throw (Exception. "No or unrecognized separators in record."))
+			\| #"\|" 
+			\, #"," 
+			\space #" "
+			nil (throw (Exception. "in determine-separator: No or unrecognized separators in record."))
 			(recur (rest line-left)))))
 
 (defn parse-date
 	[date-str]
-	date-str)
+	(let [date-nums (mapv read-string (str/split date-str #"-"))]
+		(if (not= 3 (count date-nums))
+			(throw (Exception. "in parse-date: Dates must be in MM-DD-YYYY format."))
+			(t/local-date (date-nums 2) (date-nums 0) (date-nums 1)))))
 
 (defn parse-record
 	[line]
 	(let [entries (->> (determine-separator line)
-										 (str)
-										 (re-pattern)
 										 (str/split line))]
 		(if (not= (count entries) 5)
-			(throw (Exception. "Invalid number of entries in record."))
+			(throw (Exception. "in parse-record: Invalid number of entries in record."))
 			{:last (entries 0)
 			 :first (entries 1)
 			 :gender (str/lower-case (entries 2))
 			 :fav-color (entries 3)
 			 :dob (parse-date (entries 4))})))
 
+(defn parse-records-string
+	[rec-str]
+	(mapv parse-record (str/split-lines rec-str)))
+
 (defn parse-records-file
 	[filename]
-	(->> (slurp filename)
-			 (str/split-lines)
-			 (map parse-record)))
+	(parse-records-string (slurp filename)))
 
 (defn -main
   "I don't do a whole lot ... yet."
