31b5b9cdbc77f7493827f61627d53502eb2f572b
Nerve <nervecenter7@gmail.com>
2017-03-20 22:27:12 -0400

    Documented functions. Test coverage next.

--- src/gr_homework/core.clj
+++ src/gr_homework/core.clj
@@ -5,6 +5,8 @@
   (:gen-class))
 
 (defn determine-separator
+	"Returns the separator of a record as a pattern.
+   Throws an exception a pipe, comma, or space is not found."
 	[line]
 	(loop [line-left line]
 		(condp = (first line-left)
@@ -15,6 +17,8 @@
 			(recur (rest line-left)))))
 
 (defn parse-date
+	"Parses a date string in MM-DD-YYYY into a clj-time
+   LocalDate object. Throws if it doesn't find three elements."
 	[date-str]
 	(let [date-nums (mapv read-string (str/split date-str #"-"))]
 		(if (not= 3 (count date-nums))
@@ -22,6 +26,9 @@
 			(t/local-date (date-nums 2) (date-nums 0) (date-nums 1)))))
 
 (defn parse-record
+	"Parses an individual record, or one line.
+   Expects, in order, last name, first name, gender,
+   favorite color, and date of birth."
 	[line]
 	(let [entries (->> (determine-separator line)
 										 (str/split line))]
@@ -34,10 +41,14 @@
 			 :dob (parse-date (entries 4))})))
 
 (defn parse-records-string
+	"Parses multiple records in a raw string, must contain
+   line separations."
 	[rec-str]
 	(mapv parse-record (str/split-lines rec-str)))
 
 (defn parse-records-file
+	"Parses multiple records in a file, must contain
+   line separations."
 	[filename]
 	(parse-records-string (slurp filename)))
 
