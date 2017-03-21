d5ef86c6a85439f4ff722216d201893082312ceb
Nerve <nervecenter7@gmail.com>
2017-03-20 20:49:06 -0400

    Added resource path, project instruction PDF

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot gr-homework.core
+	:resource-paths ["resources"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
