8338a91fbf79d085c2d0e8a79ccb772a967f7f11
Nerve <nervecenter7@gmail.com>
2017-03-22 14:22:13 -0400

    Split files. Handled 3 input files case. Printing funcs. Server packs.

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
 								 [org.clojure/tools.namespace "0.2.11"]
-								 [clj-time "0.13.0"]]
+								 [clj-time "0.13.0"]
+								 [http-kit "2.2.0"]
+								 [ring/ring-core "1.6.0-RC1"]
+								 [compojure "1.5.2"]]
   :main ^:skip-aot gr-homework.core
 	:resource-paths ["resources"]
   :target-path "target/%s"
--- src/gr_homework/core.clj
+++ src/gr_homework/core.clj
@@ -1,7 +1,10 @@
 (ns gr-homework.core
 	(:use [clojure.tools.namespace.repl :only (refresh)])
 	(:require [clojure.string :as str]
-						[clj-time.core :as t])
+						[clj-time.core :as t]
+						[org.httpkit.server :as http]
+						[compojure.core :refer [defroutes context GET POST ANY]]
+						[ring.middleware.content-type :refer [wrap-content-type]])
   (:gen-class))
 
 ;;
@@ -52,10 +55,19 @@
 	(mapv parse-record (str/split-lines rec-str)))
 
 (defn parse-records-file
-	"Parses multiple records in a file, must contain
+	"Parses records in a file, must contain
    line separations."
 	[filename]
 	(parse-records-string (slurp filename)))
+
+(defn parse-multiple-records-files
+	"Parses multiple records files, concats them into
+   one collection of records."
+	[& filenames]
+	(->> filenames
+			 (map slurp)
+			 (map parse-records-string)
+			 (reduce concat)))
 
 ;;
 ;; SORTING FUNCTIONS
@@ -86,11 +98,72 @@
 	[records]
 	(sort #(t/before? (:dob %1) (:dob %2)) records))
 
+;;
+;; PRINTING FUNCTIONS
+;;
+
+(defn date-str
+	[localdate]
+	(str (t/month localdate) "/" (t/day localdate) "/" (t/year localdate)))
+
 (defn print-record
 	[rec]
-	rec)
+	(do
+		(println)
+		(println "First name:      " (:first rec))
+		(println "Last name:       "  (:last rec))
+		(println "Gender:          " (:gender rec))
+		(println "Favorite color:  " (:fav-color rec))
+		(println "Date of birth:   " (date-str (:dob rec)))))
+
+(defn print-records
+	[records]
+	(dorun (map print-record records)))
+
+(defn output1
+	[records]
+	(do
+		(println)
+		(println "Sorted by gender (female first), last name ascending:")
+		(print-records (sort-by-gender-then-last-name-ascending records))))
+
+(defn output2
+	[records]
+	(do
+		(println)
+		(println "Sorted by birth date, ascending:")
+		(print-records (sort-by-dob-ascending records))))
+
+(defn output3
+	[records]
+	(do
+		(println)
+		(println "Sorted by last name, descending:")
+		(print-records (sort-by-last-name-descending records))))
+
+(defn print-three-outputs
+	[records]
+	(output1 records)
+	(output2 records)
+	(output3 records))
+
+;;
+;; REST SERVER
+;;
+
+(defroutes gr-homework-routes
+	(context "/records" req
+					 (POST "/" req (org-page-handler req))
+					 (GET "/gender" req (org-data-handler req))
+					 (GET "/birthdate" req (org-responses-handler req))
+					 (GET "/name" req (issue-page-handler req)))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  ([in1 in2 in3]
+	 (println)
+	 (println "Reading three input files...")
+	 (print-three-outputs (parse-multiple-records-files in1 in2 in3)))
+	([]
+	 (do
+		 (println "Starting server..."))))
